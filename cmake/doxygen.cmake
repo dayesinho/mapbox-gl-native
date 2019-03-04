c529c2690770b709b988e781e7431cd1ef5d76f5
Ander Conselvan de Oliveira <ander.deoliveira@mapbox.com>
2019-03-04 13:18:22 +0200

    Doxy-coverage

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,16 @@
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+    set(DOXYGEN_PROJECT_NAME "Mapbox GL Native")
+    set(DOXYGEN_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/docs")
+    set(DOXYGEN_GENERATE_XML TRUE)
+
+    doxygen_add_docs(generate_xml_doxygen
+        ${CMAKE_SOURCE_DIR}/include
+    )
+
+    add_custom_target(doxygen_coverage
+        COMMAND ${CMAKE_SOURCE_DIR}/scripts/doxy-coverage.py --noerror "${CMAKE_BINARY_DIR}/docs/xml"
+        COMMENT "Checking documentation coverage"
+        DEPENDS generate_xml_doxygen
+    )
+endif()
